20e08068f507d7eb9242dd0d7fc7d455c794a885
Jeanne Mercier <jemercie@1B1.42angouleme.fr>
2024-08-23 14:41:27 +0200

    aaa

--- main.c
+++ main.c
@@ -54,7 +54,7 @@ int main(int argc, char **argv){
 
     // check le paquet, recalculer checksum, calculer le timestamp
 
-    // faire uneroutine pour makepacket send recv, une boucle puis jsp voir comment on peut opti le truc plus tard
+    // faire une routine pour makepacket sendto recvmsg, une boucle puis jsp voir comment on peut opti le truc plus tard
 
     // need a struct to store the informations that ping display at the end
 
@@ -63,34 +63,38 @@ int main(int argc, char **argv){
 }
 
 void    fill_ip_header(struct ip *ip_header, char dest_str[100]){
-    // ip_header->ip_hl = IP_HEADER_LEN;
-    // ip_header->ip_v = IP_VERSION;
-    // ip_header->ip_tos = 0;
-    // ip_header->ip_len = IP_TOTAL_LEN;
+    ip_header->ip_hl = IP_HEADER_LEN;
+    ip_header->ip_v = IP_VERSION;
+    ip_header->ip_tos = 0;
+    ip_header->ip_len = IP_TOTAL_LEN;
 
-    // ip_header->ip_id = 0; // ??
+    ip_header->ip_id = 0; // ??
 
-    // ip_header->ip_off = SET_DONT_FRAGMENT_BIT_TRUE ;
-    // ip_header->ip_ttl = TIME_TO_LIVE
-    // ip_header->ip_p = IPPROTO_ICMP ;
+    ip_header->ip_off = SET_DONT_FRAGMENT_BIT_TRUE ;
+    ip_header->ip_ttl = TIME_TO_LIVE
+    ip_header->ip_p = IPPROTO_ICMP ;
     // ip_header->ip_sum = compute_checksum(); // -> u_short () 2 bytes
-    // ip_header->ip_src = INADDR_ANY;
-    // inet_aton(dest_str, ip_header->ip_dest.s_addr); // check les erreurs
+    ip_header->ip_src = INADDR_ANY;
+    if (!inet_aton(dest_str, ip_header->ip_dest.s_addr)) // met ip adresse bon format
+        return; // puterror eeeh
 }
 
 void    fill_icmp_message(struct icmp *icmp_message){
     icmp_message->icmp_type = ICMP_ECHO;
     icmp_message->icmp_code = ICMP_ECHOREPLY;
-    // icmp_message.icmp_cksum = compute_checksum(); // -> u_int16_t 2 bytes
     // icmp_message->icd_id = ; // mike muss did : in = getpid() & 0xFFFF pour avoir juste les 16 derniers bits
                                 // car c'est un u_int_16. C'est important poiur l'identification du paquet retour;
     // icmp_message->icd_seq= ; // nombre de paquets transmis, a ++ quand on envoie un paquet, struct principale.
-
+    
     // timestamp ? clock_gettime()
-    // data (40 bytes)
+
+    // icmp_message.icmp_cksum = compute_checksum(); // -> u_int16_t 2 bytes mais quand on a fini de remplir la struct huh
+
 }
 
 u_short compute_checksum(){
+
+
     return (0);
 }                                                                            
 
